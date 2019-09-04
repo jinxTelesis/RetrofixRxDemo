b41d2be9271206401815d04652293eeb357391ad
Andre Lussier <Dremon7@hotmail.com>
2019-09-03 21:53:40 -0400

    data classes and Deserializer added

---
+++ app/src/main/java/com/example/retrofixrxdemo/GithubRepo.java
@@ -0,0 +1,12 @@
+package com.example.retrofixrxdemo;
+
+public class GithubRepo {
+
+    String name;
+    String owner;
+    String url;
+
+    public String toString(){
+        return(name + " " + url);
+    }
+}
